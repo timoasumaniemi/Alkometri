faf5c78659964fa9f8f6fbfb79a4045b4af3c560
Timo Asumaniemi <asumaniemi.timo@outlook.com>
2023-08-30 09:54:02 +0300

    Initial setup for Alkometri and server
    Initial 'Alkometri' React app with NodeJS server

---
+++ alkometri/server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const alkometriServer = express()
+alkometriServer.use(cors())
+const port = 3001
+
+alkometriServer.get("/", (request, response) => {
+    response.status(200).json({result: "Alkometri Server responding"})
+})
+
+
+alkometriServer.listen(port,() =>{
+    console.log(`Server running at port: ${port}`)
+})

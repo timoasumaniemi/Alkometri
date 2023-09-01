0a8095005fb64ab67a18e8e194729e0ca30a7942
Timo Asumaniemi <asumaniemi.timo@outlook.com>
2023-09-01 11:34:16 +0300

    Added unit tests and refactored app.js

--- alkometri/src/App.js
+++ alkometri/src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <p>Hello Alkometri</p>
-      </header>
-    </div>
-  );
+  return(
+    <>
+      <Header header="Hello Alkometri"/>
+        <Content />
+    </>
+  )
 }
 
+function Header(props){
+  return(
+    <header className="App-header">
+    <h1>{props.header}</h1>
+    </header>
+  )
+}
+
+function Content(){
+  return(
+    <div>
+        <div className="Content-container">
+          <label for="weight-input">Weight:</label>
+          <input id="weight-input"></input>
+        </div>
+        <div className="Content-container">
+          <label for="bottles-input">Bottles:</label>
+          <input id="bottles-input"></input>
+      </div>
+    </div>
+  )
+}
 export default App;
--- alkometri/src/__tests__/App.test.js
+++ alkometri/src/__tests__/App.test.js
@@ -1,7 +1,19 @@
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import Header from '../App';
+import Content from '../App';
 
-it('Renders Hello Alkometri', () => {
-  render(<App />)
+
+it('Header should display Hello Alkometri', () => {
+  render(<Header />)
   expect(screen.getByText('Hello Alkometri')).toBeInTheDocument()
+})
+
+it('Content should have "Weight:" label', () => {
+  render(<Content />)
+  expect(screen.getByLabelText('Weight:')).toBeInTheDocument()
+})
+
+it('Content should have "Bottles:" label', () => {
+  render(<Content />)
+  expect(screen.getByLabelText('Bottles:')).toBeInTheDocument()
 })
